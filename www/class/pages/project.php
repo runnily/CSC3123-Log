3b977a0ee2e60a19dc63172e5c9d654b1d7e4b61
runnily <32201807+runnily@users.noreply.github.com>
2021-01-03 22:12:24 +0000

    changed project to adhere to php standard

--- www/class/pages/project.php
+++ www/class/pages/project.php
@@ -14,17 +14,20 @@
 
 /**
  * A traits which has code that supports adding and deleting nodes
- * and users
+ * all methods used within the handle method
  */
 trait notesAndUsers {
-    // for adding a note
-    function addNote(Context $context, $prj) 
+
+/**
+ * This function is used to add notes
+ */
+    function addNote(Context $context, $prj) : void
     {
         $formp = $context->formdata('post');
         $title = $formp->fetch('title', '', FALSE);
         if ($formp->exists('title'))
         {
-            if (trim($title) !== '') 
+            if ( trim($title) !== '') 
             {
                 $summary = $formp->fetch('summary', '', FALSE);
                 $time = (int) $formp->fetch('time', '0', FALSE);
@@ -37,10 +40,12 @@ trait notesAndUsers {
         }
     }
 
-    //for removing a note
-    function removeNote(Context $context) 
+/**
+ * This function is used to remove note
+ */
+    function removeNote(Context $context) : void
     {
-        if(count($context->rest()) == 4 && $context->rest()[2] == 'delete' && $context->rest()[1] == 'note') 
+        if ( count($context->rest()) == 4 && $context->rest()[2] == 'delete' && $context->rest()[1] == 'note') 
             {
             $trash = R::findOne('note', 'project_id = ? AND id = ?', [ $context->rest()[0], $context->rest()[3] ] );
             if ($trash) 
@@ -51,12 +56,14 @@ trait notesAndUsers {
         }
     }
 
-    //for adding a user to a project
-    function addUser(Context $context, $prj) 
+/**
+ * This function is used to add a user
+ */
+    function addUser(Context $context, $prj) : void
     {
         $formuser = $context->formdata('post');
         $username = $formuser->fetch('username', '', FALSE);
-        if (strlen($username)>0) 
+        if ( strlen($username)>0 ) 
         {
             $bool = $formuser->fetch('admin', 0, FALSE) === 'TRUE' ?  TRUE : FALSE;
             try {
@@ -75,10 +82,12 @@ trait notesAndUsers {
         }
     }
 
-    //for deleting a user
-    function removeUser(Context $context, $prj) 
+/**
+ * This function is used to remove a user
+ */
+    function removeUser(Context $context, $prj) :void
     {
-        if(count($context->rest()) == 4 && $context->rest()[2] == 'delete' && $context->rest()[1] == 'user' )
+        if ( count($context->rest()) == 4 && $context->rest()[2] == 'delete' && $context->rest()[1] == 'user' )
         {
             $user_id = $context->rest()[3];
             $project_id = $context->rest()[0];
@@ -91,12 +100,14 @@ trait notesAndUsers {
         }
     }
     
-    //for deleting project
-    function removePrj(Context $context, $prj) 
+/**
+ * This is used for deleting projects
+ */
+    function removePrj(Context $context, $prj) : void
     {
         try 
         {
-            if(count($context->rest()) == 2 && $context->rest()[1] == 'delete')
+            if ( count($context->rest()) == 2 && $context->rest()[1] == 'delete')
             {
                 R::trash($prj);
                 $context->divert("/");
@@ -129,32 +140,36 @@ trait notesAndUsers {
             $context->local()->addval("projectid", $project); 
 
             $prj = R::findOne('project', 'id = ?', [$project] );
-            if ($prj) 
+            if ($prj) // if we have a project 
             { 
+                // passing values to twig to show the project information
                 $context->local()->addval('projectName', $prj->pname);
                 $context->local()->addval('projectSummary', $prj->summary); 
                 $context->local()->addval("exits", 1); 
                 $notes = R::find('note', 'project_id = ?', [$project] );
                 $context->local()->addval("deletePrj", "/project/{$project}/delete"); 
 
-                if ($notes) 
+                if ($notes) // if we have a note
                 {
+                    // passing values to twig to show note information
                     $context->local()->addval("notes", $notes); 
                     $context->local()->addval("currentNote", "/project/{$project}/note"); 
                 }
                 
                 $users = R::getAll('SELECT user.login, user.id, manage.admin FROM user, manage WHERE user.id = manage.u_id AND manage.project_id = :project_id',
                     [':project_id' => "{$project}"]
-                );
+                ); // get all users names and ids assoicated with the project
 
-                if ($users) 
+                if ($users) // if we have users
                 {
+                    // pass the values if we have a list of users
                     $context->local()->addval("users", $users); 
                     $context->local()->addval("currentUser", "/project/{$project}/user");
                 }
                 $context->local()->addval('project_id', $project); 
 
                 try {
+                    // try performing these action
                     $this->addUser($context, $prj);
                     $this->removeNote($context);
                     $this->addNote($context, $prj);
@@ -162,6 +177,7 @@ trait notesAndUsers {
                     $this->removePrj($context, $prj);
                 } catch (\Exception $e) 
                 {
+                    // else catch the exception and display a message
                     $context->local()->message(local::MESSAGE, "Something went wrong!");
                 }
             }
