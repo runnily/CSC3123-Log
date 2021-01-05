6486185e983806df3a8f61a890330b7610f534d7
runnily <32201807+runnily@users.noreply.github.com>
2021-01-05 00:52:28 +0000

    added fresh

--- www/class/pages/home.php
+++ www/class/pages/home.php
@@ -31,11 +31,12 @@
             if ($context->hasUser())
             {   
                 $prj = [];
-                //$context->user()->fresh();
+                
                 $mng = $context->user()->ownManage;
                 $userNotesTotal = 0;
                 foreach ($mng as $m) 
                 {
+                    $m = $m->fresh();
                     $p = R::load('project', $m->project_id);
                     $prj[$p->pname] = $p->contributions();
                     $userNotesTotal += $p->contributions();
